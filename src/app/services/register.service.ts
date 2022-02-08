902181561bd47743939c64d7823f42d97cdb1ccd
OBeratis <odysseus.beratis@de.experis.com>
2022-02-08 14:54:34 +0100

    FEAT: Register improvement for user

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,10 +1,15 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { finalize, map} from 'rxjs/operators';
+import { catchError, of, tap, throwError  } from 'rxjs';
 
 import { environment } from '../../environments/environment'
+import { TrainerPagePage } from '../pages/trainer-page/trainer-page.page';
+import { Trainer, TrainerResponse } from '../models/pokemon.model';
 
 const {pokemonApiBaseUrl} = environment;
+const { pokemonSessionKeyUser } = environment;
 
 @Injectable({
     providedIn: 'root'
@@ -33,4 +38,45 @@ export class RegisterService {
             headers
         })
     }
+
+    // Register new user
+    newRegister(username: string): Observable<boolean>{
+        const user = {
+            username, pokemon: []
+        }
+
+        const headers = this.createHeaders();
+
+        return this.http
+        .post<TrainerResponse<Trainer>>(`${pokemonApiBaseUrl}`, user, {
+          headers,
+        })
+        .pipe(
+  
+          tap((response) => {
+  
+            if (response.success === false) {
+              // Throw a new error to be handled in the catchError()
+              throwError(() => new Error(response.error));
+            }
+            // Cause side effects in a tap. (like saving to storage).
+            const user: Trainer = response.data;
+            // localStorage.setItem(pokemonSessionKeyUser, JSON.stringify(user));
+  
+            // Note: Tap can not change the response, so no return is allowed.
+          }),
+  
+          map((response: TrainerResponse<Trainer>) => {
+            // Skips if there was an error thrown
+            return response.success
+          }),
+          
+          // Catch the error and use the "of" operator 
+          // Throw the message we want to display in the component.
+          catchError((error) => {
+            throw error.error.error;
+          })
+        );           
+    }
 }
+
